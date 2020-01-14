d56c363ff913f0afe8c52722bdbc016ef08ec98c
Dieggo Carrilho <dieggopsc@gmail.com>
2020-01-14 13:04:11 -0300

    Estruturação de arquitetura

---
+++ src/main/java/com/desafio/CarUserSystem/controller/AuthController.java
@@ -0,0 +1,9 @@
+package com.desafio.CarUserSystem.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/signin")
+public class AuthController {
+
+}
---
+++ src/main/java/com/desafio/CarUserSystem/security/JwtAuthorizationFilter.java
@@ -0,0 +1,4 @@
+package com.desafio.CarUserSystem.security;
+
+public class JwtAuthorizationFilter
+}
